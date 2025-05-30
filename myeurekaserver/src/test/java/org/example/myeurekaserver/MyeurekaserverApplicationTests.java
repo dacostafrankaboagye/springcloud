fd047c48c0b133e15d40ede3cfac4127e52a9ef0
frankkwabenaaboagye <frank.aboagye@amalitech.com>
2025-05-30 12:50:21 +0000

    first commit

---
+++ myeurekaserver/src/test/java/org/example/myeurekaserver/MyeurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.myeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyeurekaserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
